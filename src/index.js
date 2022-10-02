ca221ae0f73ec7fd3876ad55a29fc27b1be3c091
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2022-10-02 09:29:43 -0300

    create .ignore

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const Router = express.Router();
+
+Router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+
+Router.get('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+app.use(Router)
+
+app.listen(3333, () => {
+    console.log('http://localhost:3333');
+})
+
+
+
